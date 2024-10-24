3f3c6496dfa4541efea310ec1be4abb7a58d0f10
BrunoSerpa <serpabruno2005@gmail.com>
2024-10-24 12:17:46 -0300

    Atualizei a conexão do mongo

--- Programa/conexaoBanco/conectarMongo.py
+++ Programa/conexaoBanco/conectarMongo.py
@@ -1,12 +1,11 @@
-from Programa.conexaoBanco.configuracao import carregarConfiguracao
 from Programa.funcoes.utils.salvarErro import salvarErro
 
 from pymongo import MongoClient
 
-def conectar():
+def conectarMongo():
     try:
-        cliente = MongoClient("linkMongo")
+        cliente = MongoClient("linkmongodb")
         return cliente.Loja_Online
-    except Exception as e:
+    except Exception as e:  
         salvarErro("Erro ao conectar ao Mongo", e)
         return None
